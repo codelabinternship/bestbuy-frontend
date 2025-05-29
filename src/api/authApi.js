7012367586dd9caebe786116e654eec678ae79ae
ibrohim-001 <rustamovibrohimjon62@hmail.com>
2025-05-29 14:51:53 +0500

    commit

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,7 +1,6 @@
 // src/api/authApi.js
 import axios from "axios";
-
-const API_URL = "http://192.168.28.10:8000";
+const API_URL = "http://192.168.174.177:8000";
 
 export const PostData = async (data, tree) => {
   console.log(data);
@@ -9,11 +8,13 @@ export const PostData = async (data, tree) => {
   const response = await axios.post(`${API_URL}${tree}`, data);
   return response.data;
 };
-
+  
 export const GetData = async (tree) => {
   const response = await axios.get(`${API_URL}${tree}`);
   return response.data;
 };
+
+
 
 export const DeleteData = async (tree, id) => {
   const response = await axios.delete(`${API_URL}${tree}${id}`);
