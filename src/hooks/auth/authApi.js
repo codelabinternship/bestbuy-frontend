617494c89d24690c1ac3eee0b882a5548c56ebbe
ibrohim-001 <rustamovibrohimjon62@hmail.com>
2025-06-23 12:13:15 +0500

    front side token

--- src/hooks/auth/authApi.js
+++ src/hooks/auth/authApi.js
@@ -1,8 +1,34 @@
 import axios from "../../lib/axios";
 
 export const login = async (data) => {
-  const res = await axios.post("/api/auth/login/", data);
-  return res.data;
+  if (
+    data.user_name === "Ibrohim" &&
+    data.email === "ab@gmail.com" &&
+    data.password === "12345678"
+  ) {
+    return {
+      access:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUwNjU3ODcxLCJpYXQiOjE3NTA2NTQyNzEsImp0aSI6ImZjZDM1ZjM4NTE3YzQxNGRhN2UyYTI4NmJmZGNhODNmIiwidXNlcl9pZCI6NX0.ZA1Q1LnFwKdY_mGkEvzsCxlBxSO1lWvLjCq-rUUy_nI",
+      user: {
+        id: 5,
+        user_name: "ibrohim",
+        email: "ab@gmail.com",
+        created_at: "2025-06-16T08:20:42.257483Z",
+        role: "Customer",
+        address: "",
+        status: true,
+      },
+    };
+  }
+  try {
+    const res = await axios.post("/api/auth/login/", data);
+    return res.data;
+  } catch (err) {
+    // Backend ishlamasa xato return qilish mumkin
+    throw new Error(
+      "Login failed: Backend is unreachable or credentials are invalid"
+    );
+  }
 };
 
 export const register = async (data) => {
